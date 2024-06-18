c42717284ce547c22724bc3ab671681a077849dc
MateusMayan <mateusmayan@icloud.com>
2024-06-18 09:58:02 -0300

    Servico de mensagens implementado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
@@ -14,12 +15,17 @@ export class LoginComponent implements OnInit {
       senha: ''
   }
 
-  email = new FormControl(null, Validators.email)
-  senha = new FormControl(null, Validators.minLength(3))
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  logar() {
+    this.toast.error('Usuário e/ou senha inválidos!', "Login");
+    this.creds.senha = '';
   }
 
   validaCampos(): boolean {
